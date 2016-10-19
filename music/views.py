8160cd435c421a52c65933d4984ea92019dd6beb
Pawel <pawel.miron@gmail.com>
2016-10-19 23:52:29 +0200

    commit

--- music/views.py
+++ music/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("<h1>This is the music homepage</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
