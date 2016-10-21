c84ba98bdd99ed3e40affa584d165270977b07d0
Pawel <pawel.miron@gmail.com>
2016-10-22 00:11:48 +0200

    31 ModelForm and CreateView

--- music/urls.py
+++ music/urls.py
@@ -4,13 +4,12 @@ from . import views
 app_name ='music'
 
 urlpatterns = [
-    # /music/
-    url(r'^$', views.index, name='index'),
 
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetalView.as_view(), name='detail'),
 
-    # /music/<album_id>/favourite
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,30 +1,21 @@
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
+from .models import Song
 
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-from .models import Album, Song
-from django.shortcuts import render, get_object_or_404
+    def get_queryset(self):
+        return Album.objects.all()
 
+class DetalView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
-def index(request):
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist' , 'album_title', 'genre', 'album_logo']
 
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
-
-
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song =album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {'album': album, 'error_message': "You did not select any song",})
-    else:
-        if (selected_song.is_favourite):
-            selected_song.is_favourite= False
-        else:
-            selected_song.is_favourite=True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
